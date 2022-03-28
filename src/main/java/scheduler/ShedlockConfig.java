e3fce730a454e624d98e37820edb50a214fe51d6
LiewShanWei <shanweiliew93@gmail.com>
2022-03-28 18:02:00 +0800

    Added database name to variable

--- src/main/java/scheduler/ShedlockConfig.java
+++ src/main/java/scheduler/ShedlockConfig.java
@@ -4,6 +4,7 @@ import com.mongodb.client.MongoClient;
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.mongo.MongoLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,8 +13,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableSchedulerLock(defaultLockAtMostFor = "15M", defaultLockAtLeastFor = "PT1M")
 public class ShedlockConfig {
+    @Value("${spring.data.mongodb.database}")
+    private String databaseName;
+
     @Bean
     public LockProvider lockProvider(MongoClient mongo) {
-        return new MongoLockProvider(mongo.getDatabase("shedlock-test"));
+        return new MongoLockProvider(mongo.getDatabase(databaseName));
     }
 }
